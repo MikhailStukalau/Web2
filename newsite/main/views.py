d23fde7551cefab8b05e5f4523959131e04e09dd
muxar <1>
2022-10-07 00:41:11 +0300

    start_files

--- newsite/main/views.py
+++ newsite/main/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
     return render(request, 'index.html')
 
 def about(request):
-    return HttpResponse("ABOUT US!")
+    return render(request, 'about.html')
+
+def contacts(request):
+    return render(request, 'contacts.html')
