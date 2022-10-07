1f834615e5de12e10854a467727a19bca314a05d
muxar <1>
2022-10-07 12:36:26 +0300

    start_files

--- newsite/main/models.py
+++ newsite/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField("TITLE", max_length=250)
+    task = models.TextField("описание")
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
